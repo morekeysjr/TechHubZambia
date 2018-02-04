080014fac80416614be0f00bf5d52352cc5d56c5
morekeysjr <36127563+morekeysjr@users.noreply.github.com>
2018-02-04 12:55:33 +0200

    Add files via upload
    1 Data Collection Criteria
    2 Exponential Smoothing Logic
    3 Visual Display of Smoothed Data
    4 Regression Logic

---
+++ Project.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 24 04:55:21 2017
+
+@author: Emmanuellwelejr
+In this project, I have written a program that will use the exponential smoothing 
+to help you predict a value in the future (e.g future stock).  
+In addition, the program comes up with the linear regression equation 
+to predict the same value as was done in exponential smoothing. 
+Overview
+Use forecasting today to optimize for tomorrow! 
+Time series forecasting is the use of a model to predict future values 
+based on previously observed values.
+It is one of the prime tools of any business analyst used to predict demand 
+and inventory, budgeting, sales quotas, marketing campaigns, 
+and procurement. Accurate forecasts lead to better decisions. 
+Current visual implements well known exponential smoothing method for the forecasting. 
+The prediction is based on trend and seasonality modelling. 
+You can control the algorithm parameters and the visual attributes to suit your needs.
+
+"""
+
+import numpy as np
+from matplotlib import pyplot as plt
+
+
+def exponential_smoothing(alpha, s):
+    s2 = np.zeros(s.shape)
+    s2[0] = s[0]
+    for i in range(1, len(s2)):
+        s2[i] = alpha*s[i]+(1-alpha)*s2[i-1]
+    return s2
+
+
+def show_data(new_year, pre_year, data, s_pre_double, s_pre_triple):
+    year, time_id, number = data.T
+
+    plt.figure(figsize=(14, 6), dpi=80)
+    plt.plot(year, number, color='blue', label="actual value")
+    plt.plot(new_year[1:], s_pre_double[2:],
+             color='red', label="double predicted value")
+    plt.plot(new_year[1:], s_pre_triple[2:],
+             color='green', label="triple predicted value")
+    plt.legend(loc='lower right')
+    plt.title('Projects')
+    plt.xlabel('year')
+    plt.ylabel('number')
+    plt.xticks(new_year)
+    # plt.colors()
+    plt.show()
+
+
+def main():
+    alpha = .70
+    pre_year = np.array([2016, 2017])
+    data_path = r'data1.txt'
+    data = np.loadtxt(data_path)
+    year, time_id, number = data.T
+    initial_line = np.array([0, 0, number[0]])
+    initial_data = np.insert(data, 0, values=initial_line, axis=0)
+    initial_year, initial_time_id, initial_number = initial_data.T
+
+    s_single = exponential_smoothing(alpha, initial_number)
+    s_double = exponential_smoothing(alpha, s_single)
+
+    a_double = 2*s_single-s_double
+    b_double = (alpha/(1-alpha))*(s_single-s_double)
+    s_pre_double = np.zeros(s_double.shape)
+    for i in range(1, len(initial_time_id)):
+        s_pre_double[i] = a_double[i-1]+b_double[i-1]
+    pre_next_year = a_double[-1]+b_double[-1]*1
+    pre_next_two_year = a_double[-1]+b_double[-1]*2
+    insert_year = np.array([pre_next_year, pre_next_two_year])
+    s_pre_double = np.insert(s_pre_double, len(s_pre_double), values=np.array(
+        [pre_next_year, pre_next_two_year]), axis=0)
+
+    s_triple = exponential_smoothing(alpha, s_double)
+
+    a_triple = 3*s_single-3*s_double+s_triple
+    b_triple = (alpha/(2*((1-alpha)**2)))*((6-5*alpha)*s_single -
+                                           2*((5-4*alpha)*s_double)+(4-3*alpha)*s_triple)
+    c_triple = ((alpha**2)/(2*((1-alpha)**2)))*(s_single-2*s_double+s_triple)
+
+    s_pre_triple = np.zeros(s_triple.shape)
+
+    for i in range(1, len(initial_time_id)):
+        s_pre_triple[i] = a_triple[i-1]+b_triple[i-1]*1 + c_triple[i-1]*(1**2)
+
+    pre_next_year = a_triple[-1]+b_triple[-1]*1 + c_triple[-1]*(1**2)
+    pre_next_two_year = a_triple[-1]+b_triple[-1]*2 + c_triple[-1]*(2**2)
+    insert_year = np.array([pre_next_year, pre_next_two_year])
+    s_pre_triple = np.insert(s_pre_triple, len(s_pre_triple), values=np.array(
+        [pre_next_year, pre_next_two_year]), axis=0)
+
+    new_year = np.insert(year, len(year), values=pre_year, axis=0)
+    output = np.array([new_year, s_pre_double, s_pre_triple])
+    print(output)
+    show_data(new_year, pre_year, data, s_pre_double, s_pre_triple)
+
+
+if __name__ == '__main__':
+    main()
+
